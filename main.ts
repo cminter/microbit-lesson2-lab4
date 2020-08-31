46124ecd62d516aac303eec539ccc5ed450b3575
Corey Minter <corey.minter@gmail.com>
2020-08-30 17:41:16 -0700

    implemented melting and refactor variable names

--- main.ts
+++ main.ts
@@ -1,36 +1,67 @@
-function dropFlake () {
-    flakeCol = randint(0, 4)
-    flakeSize = randint(10, 50)
-    flakeRow = -1
+function meltSome () {
+    col = randint(0, 4)
+    size = randint(10, 50)
+    row = 0
     dropping = 1
-    while (dropping == 1 && flakeRow < 4) {
-        prevFlakeRow = flakeRow
-        flakeRow += 1
-        total = led.pointBrightness(flakeCol, flakeRow)
-        if (total == 0) {
-            led.unplot(flakeCol, prevFlakeRow)
-            led.plotBrightness(flakeCol, flakeRow, flakeSize)
+    while (dropping == 1 && row <= 4) {
+        current = led.pointBrightness(col, row)
+        if (current > 0) {
+            led.plotBrightness(col, row, Math.max(current - size, 0))
+            dropping = 0
+        }
+        row += 1
+    }
+}
+function dropFlake () {
+    col = randint(0, 4)
+    size = randint(10, 50)
+    row = -1
+    dropping = 1
+    while (dropping == 1 && row < 4) {
+        prevRow = row
+        row += 1
+        current = led.pointBrightness(col, row)
+        if (current == 0) {
+            led.unplot(col, prevRow)
+            led.plotBrightness(col, row, size)
         } else {
-            if (total < 255) {
-                led.unplot(flakeCol, prevFlakeRow)
-                led.plotBrightness(flakeCol, flakeRow, total + flakeSize)
+            if (current < 255) {
+                led.unplot(col, prevRow)
+                led.plotBrightness(col, row, current + size)
             }
             dropping = 0
         }
-        basic.pause(100)
+        basic.pause(50)
     }
 }
-let total = 0
-let prevFlakeRow = 0
+input.onButtonPressed(Button.A, function () {
+    if (snowing < 1) {
+        snowing = 1
+    } else {
+        snowing = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (snowing > -1) {
+        snowing = -1
+    } else {
+        snowing = 0
+    }
+})
+let prevRow = 0
+let current = 0
 let dropping = 0
-let flakeRow = 0
-let flakeSize = 0
-let flakeCol = 0
-let snowing = 1
+let row = 0
+let size = 0
+let col = 0
+let snowing = 0
+snowing = 1
 basic.clearScreen()
 basic.forever(function () {
-    while (snowing == 1) {
+    if (snowing == 1) {
         dropFlake()
-        basic.pause(100)
+    } else if (snowing == -1) {
+        meltSome()
     }
+    basic.pause(100)
 })
